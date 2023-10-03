b1a34a9523f5cddb7bb9eb3fda0f5d484def1a08
Jafet Alvarado <56947979+jafetal@users.noreply.github.com>
2023-10-02 21:22:38 -0600

    base template

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,16 @@
+import Image from "next/image";
+
 // `app/page.tsx` is the UI for the `/` URL
 export default function Page() {
-  return <h1>Hola buenas tardes como estás</h1>
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="text-center">
+        <div><h1 className="hola-text">Hola</h1></div>
+        <p className="buenas-text">Buenas tardes</p>
+        <div className='flex justify-center mt-5'>
+          <Image className="rounded-3xl" src="/don-cangrejo.jpg" alt='cangrejo' width={150} height={100}></Image>
+        </div>
+      </div>
+    </div>
+  )
 }
